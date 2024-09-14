f2912c885cec4f1046dbe865c2c3e3abe90bbde5
e1327906 <e1327906@u.nus.edu>
2024-09-14 16:43:10 +0800

    changes for external url

--- src/services/ApiUtils.ts
+++ src/services/ApiUtils.ts
@@ -4,7 +4,7 @@ import { UserLoginData, UserRegistrationData, FeedbackData } from './types';
 const apiClient = axios.create({
   //baseURL: 'http://eb-sqrts-env.eba-bq53g3td.ap-southeast-1.elasticbeanstalk.com/tg_query_api/api/v1/',
   //baseURL: 'http://localhost:31000/',
-  baseURL: 'http://qr-gen-service:8080/',
+  baseURL: 'http://a240ef9d8bdbf4b738905bbd2dfe1c01-1547472837.ap-southeast-1.elb.amazonaws.com:8080/',
   // add more default settings here
 });
 
